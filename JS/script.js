d40ddb1a9df7ba189a802a2bf462c08b26f89b0f
Silas Chan <silaschan421@gmail.com>
2021-05-24 20:05:46 +0800

    modified css

--- JS/script.js
+++ JS/script.js
@@ -2,16 +2,19 @@
 const link = document.querySelector(".link-list");
 const linkBtn = document.querySelector(".link-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '80px',
+//     duration: 2000,
+//     reset: true
+// })
       
 linkBtn.onclick = ()=> {
     link.classList.add("active")
-    linkBtn.classList.add("hide")
+    linkBtn.classList.add("hide") 
 }
 
 cancelBtn.onclick = ()=> {
     link.classList.remove("active")
     linkBtn.classList.remove("hide")
-
 }
-
-
